e64cb9a173ffe4b91f8d05b844b62dfdacbc8699
Lucas <lucaswillyaraujo@gmail.com>
2024-05-19 12:03:10 -0300

    exercicio-3

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
-const calcularInss = require("./calculo_inss");
+const readline = require('readline')
+const PdfDocument = require('pdfkit')
+const fs = require('fs')
 
-console.log(calcularInss(8000))
+const input = readline. createInterface(
+    process.stdin,
+    process.stdout
+);
+
+input.question("Nome do funcionário: ", (nomeDigitado) => {
+    const doc = new PdfDocument()
+    doc.pipe(fs.createWriteStream('holerite.pdf'))
+
+    doc.fontSize(16)
+    
+    doc.text("Folha de Pagamento")
+    doc.text(`Funcionário: ${nomeDigitado}`)
+
+
+    doc.end()
+})
+
+
+//const calcularInss = require("./calculo_inss");
+
+//console.log(calcularInss(8000))
